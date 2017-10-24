63dcf6004aa5465ffbb0255d69c5eaa19422f8c9
Vivek Jaiswal <vivekjaiswal@live.in>
2017-10-25 00:02:25 +0530

    added solution of filtered ls

---
+++ filtered-ls.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function doneReading(err, list) {
+	if (err) {
+		console.log(err)
+	}
+	for (var i = 0; i < list.length; i++) {
+		if (path.extname(list[i]) === ('.' + process.argv[3])) {
+			console.log(list[i])
+		}
+	}
+})
